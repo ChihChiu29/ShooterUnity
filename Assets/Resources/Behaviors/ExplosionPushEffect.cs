6ad373083a09d3871df189defb61c86034d5f824
Chih Chiu <chih.chiu.19@gmail.com>
2017-06-07 23:22:12 -0700

    basic explosion push effec done

---
+++ Assets/Resources/Behaviors/ExplosionPushEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionPushEffect : MonoBehaviour
+{
+  // Push force goes with 1/r.
+  public float pushForceScale = 3f;
+
+  void OnTriggerEnter2D (Collider2D coll)
+  {
+    ApplyForce (coll);
+  }
+
+  void OnTriggerStay2D (Collider2D coll)
+  {
+    ApplyForce (coll);
+  }
+
+  private void ApplyForce (Collider2D coll)
+  {
+    GameObject victim = coll.gameObject;
+    Vector2 explosionCenter = 
+      new Vector2 (transform.position.x, transform.position.y);
+    Vector2 victimCenter =
+      new Vector2 (victim.transform.position.x, victim.transform.position.y);
+    Vector2 direction = victimCenter - explosionCenter;
+    float distance = direction.magnitude;
+
+    Rigidbody2D rigidbody = coll.gameObject.GetComponent<Rigidbody2D> ();
+    rigidbody.AddForce (direction / (distance * distance) * pushForceScale);
+  }
+}
